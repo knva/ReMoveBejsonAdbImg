47dcfe32125c0c939ba9dcc035b5e1b2e3c4882d
Lee Petter <txt_ch@163.com>
2020-01-06 12:05:22 +0800

    Create 删除恶心hxd的bejson adblock图片.user.js

---
+++ 删除恶心hxd的bejson adblock图片.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         删除恶心hxd的bejson adblock图片
+// @namespace    hxd
+// @version      0.1
+// @description  删除恶心hxd的bejson adblock图片
+// @author       You
+// @match        http://*.bejson.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.killads = "hello";
+})();
